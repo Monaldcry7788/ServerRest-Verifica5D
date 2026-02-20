3ec20376f4b7fc3a7fe70b88f5ff8f0b83526cb7
Monald <monaldi.christian27@gmail.com>
2026-02-20 09:45:38 +0100

    Aggiorna la documentazione degli endpoint per la Roulette REST API e modifica i parametri richiesti

--- src/serverrest/ServerRest.java
+++ src/serverrest/ServerRest.java
@@ -52,8 +52,8 @@ public class ServerRest {
             System.out.println("Porta: " + porta);
             System.out.println();
             System.out.println("Endpoint disponibili:");
-            System.out.println("  - POST: http://localhost:" + porta + "/DA FARE");
-            System.out.println("  - GET:  http://localhost:" + porta + "/DA FARE");
+            System.out.println("  - POST: http://localhost:" + porta + "/api/roulette/paridispari/post");
+            System.out.println("  - GET:  http://localhost:" + porta + "/api/roulette/paridispari/get");
             System.out.println("  - Info: http://localhost:" + porta + "/");
             System.out.println();
             System.out.println();
@@ -76,21 +76,20 @@ public class ServerRest {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         
         Map info = new HashMap<>();
-        info.put("messaggio", "Benvenuto alla Calcolatrice REST API");
-        info.put("versione", "2.0.0");
+        info.put("messaggio", "Benvenuto alla Roulette REST API");
+        info.put("versione", "1.0.0");
         info.put("tecnologia", "Java + GSON");
         
         Map endpoints = new HashMap<>();
-        endpoints.put("POST", "/api/calcola/post");
-        endpoints.put("GET", "/api/calcola/get?operando1=X&operando2=Y&operatore=OP");
+        endpoints.put("POST", "/api/roulette/paridispari/post");
+        endpoints.put("GET", "/api/roulette/paridispari/get?giocata=PARI&numero=5");
         info.put("endpoints", endpoints);
         
-        Map operatori = new HashMap<>();
-        operatori.put("somma", "SOMMA o +");
-        operatori.put("sottrazione", "SOTTRAZIONE o -");
-        operatori.put("moltiplicazione", "MOLTIPLICAZIONE o * o X");
-        operatori.put("divisione", "DIVISIONE o /");
-        info.put("operatori_supportati", operatori);
+        Map parametri = new HashMap<>();
+        parametri.put("giocata", "PARI o DISPARI");
+        parametri.put("numero", "Un numero da 0 a 36");
+
+        info.put("parametri richiesti", parametri);
         
         String jsonRisposta = gson.toJson(info);
         
