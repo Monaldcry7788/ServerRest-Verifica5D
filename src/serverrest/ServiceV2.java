f024b7af4034388b2c467b08fc50528fe262f5ea
Monald <monaldi.christian27@gmail.com>
2026-02-20 10:07:31 +0100

    Aggiorna ServiceV2 per supportare la logica di calcolo basata su 'ROSSO' e 'NERO'; modifica i metodi per utilizzare RequestV2 e aggiorna la validazione dei parametri

--- src/serverrest/ServiceV2.java
+++ src/serverrest/ServiceV2.java
@@ -5,13 +5,29 @@
 package serverrest;
 
 import serverrest.parsers.RequestV1;
+import serverrest.parsers.RequestV2;
+
+import java.util.HashSet;
+import java.util.Map;
 
 /**
  *
  * @author delfo
  */
 public class ServiceV2 {
-    
+
+    private static final HashSet<String> rosso = new HashSet<>() {{
+        add("1"); add("3"); add("5"); add("7"); add("9"); add("12"); add("14"); add("16"); add("18");
+        add("19"); add("21"); add("23"); add("25"); add("27"); add("30"); add("32"); add("34");
+        add("36");
+    }};
+
+    private static final HashSet<String> nero = new HashSet<>() {{
+        add("2"); add("4"); add("6"); add("8"); add("10"); add("11"); add("13"); add("15"); add("17");
+        add("20"); add("22"); add("24"); add("26"); add("28"); add("29"); add("31"); add("33");
+        add("35");
+    }};
+
     /**
      * Esegue l'operazione matematica richiesta
      * 
@@ -21,22 +37,26 @@ public class ServiceV2 {
      * @return 
      * @throws IllegalArgumentException se ...
      */
-    public static boolean logicaDiCalcolo(RequestV1 request) throws IllegalArgumentException {
-        
+    public static boolean logicaDiCalcolo(RequestV2 request) throws IllegalArgumentException {
+
         // Controllo se i parametri passati sono validi
         if (!parametriValidi(request)) {
-            throw new IllegalArgumentException("Dati inseriti non validi. La giocata deve essere 'PARI' o 'DISPARI' e il numero deve essere un intero compreso tra 0 e 36.");
+            throw new IllegalArgumentException("Dati inseriti non validi. La giocata deve essere 'ROSSO' o 'NERO', il numero deve essere un intero compreso tra 0 e 36 e la puntata maggiore di 0.0");
         }
 
         Integer numero = Integer.parseInt(request.getNumero());
         if (numero == 0)
             return false;
-        return (request.getGiocata().equalsIgnoreCase("PARI") && numero % 2 == 0) || (request.getGiocata().equalsIgnoreCase("DISPARI") && numero % 2 != 0);
+
+        if (request.getGiocata().equalsIgnoreCase("ROSSO"))
+            return rosso.contains(request.getNumero());
+        else
+            return nero.contains(request.getNumero());
     }
 
     // Metodo di validazione dei parametri (da implementare)
-    private static boolean parametriValidi(RequestV1 request)
+    private static boolean parametriValidi(RequestV2 request)
     {
-        return !request.getGiocata().isEmpty() && (request.getGiocata().equalsIgnoreCase("PARI") || request.getGiocata().equalsIgnoreCase("DISPARI") && !request.getNumero().isEmpty() && Integer.parseInt(request.getNumero()) <= 36);
+        return !request.getGiocata().isEmpty() && (request.getGiocata().equalsIgnoreCase("ROSSO") || request.getGiocata().equalsIgnoreCase("NERO")) && !request.getNumero().isEmpty() && Integer.parseInt(request.getNumero()) <= 36 && request.getImporto() > 0.0;
     }
 }
